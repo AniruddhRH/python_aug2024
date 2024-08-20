a7f21d5a5e96533aabec377fc93778d0112b2249
AniruddhRH <aniruddhahallur16@gmail.com>
2024-08-20 17:39:40 +0530

    program p6 added

--- python_learning/p3.py
+++ python_learning/p3.py
@@ -1,4 +1,4 @@
-#Accept a number as input, say X and define a logic to get the output say Y. The Input can be only 0 or 1 and the output must be 1 if input is 0 and vicevesa. Do not use Boolean Alzebra
+#Accept a number as input, say X and define a logic to get the output say Y. The Input can be only 0 or 1 and the output must be 1 if input is 0 and vicevesa. Do not use Boolean Alzeb
 
 X = int(input('Enter the input number(0 or 1 only:)'))
 
